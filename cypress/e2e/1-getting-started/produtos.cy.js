cdbb551c5a0d499fa3741ae6b5aabef6c8b6996e
marllondallas <marllon_dallas@hotmail.com>
2023-01-07 00:11:17 -0300

    alterações nos testes automatizados

---
+++ cypress/e2e/1-getting-started/produtos.cy.js
@@ -0,0 +1,29 @@
+describe("Funcionalidade página de produtos", () => {
+  beforeEach(() => {
+    cy.visit("http://lojaebac.ebaconline.art.br/produtos/"); // Acessa a página de login
+  });
+
+  it("Deve selecionar um produto da lista", () => {
+    cy.get('[class="product-block grid"]')
+           // .eq(3)
+            .contains('Aero Daily Fitness Tee')
+            .click(); // Clica no produto
+  });
+
+    it.only("Deve selecionar um produto ao carrinho", () => {      
+       let quantidade = 4;
+        cy.get('[class="product-block grid"]')
+            .contains('Ajax Full-Zip Sweatshirt')
+            .click(); // Clica no produto
+        cy.get('.button-variable-item-S').click();
+        cy.get('.button-variable-item-Green').click();
+        cy.get('.input-text').clear() .type(quantidade);
+        cy.get('.single_add_to_cart_button').click();
+        cy.get('.woocommerce-message').should('contain', 'foram adicionados no seu carrinho.');
+        
+
+    });
+
+});
+
+
