28b9e431dd21d0bb1697d26a8286390546af3042
marllondallas <marllon_dallas@hotmail.com>
2023-01-08 21:43:56 -0300

    mudando a versão do cypres para a versão 10.7

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,16 @@
 module.exports = {
+ 
+
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    
+   config.baseUrl = "http://lojaebac.ebaconline.art.br/";
+      
+      
+    
+      
     },
   },
 };
