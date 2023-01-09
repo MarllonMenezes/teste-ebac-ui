953371da5b2304a5a68362b96125726d30c6b44c
marllondallas <marllon_dallas@hotmail.com>
2023-01-08 21:44:41 -0300

    att de versão do cypress

---
+++ cypress/e2e/integration/tela-inicial.cy.js
@@ -0,0 +1,31 @@
+describe("Funcionalidade Login", () => {
+  
+    it.only("Login com sucesso", () => {
+    cy.visit("http://lojaebac.ebaconline.art.br/minha-conta");
+    cy.get("#username").type("alu@teste.com"); // Digita o usuário
+    cy.get("#password").type("teste@teste.com"); // Digita a senha
+    cy.get(".woocommerce-form > .button").click(); // Clica no botão de login
+  });
+});
+
+it("deve exibir mensagem de erro ao inserir usuário inválido", () => {
+  cy.visit("http://lojaebac.ebaconline.art.br/minha-conta");
+  cy.get("#username").type("alu@teste.com"); // Digita o usuário
+  cy.get("#password").type("teste@teste.com"); // Digita a senha
+  cy.get(".woocommerce-form > .button").click(); // Clica no botão de login
+  cy.get(".woocommerce-error").should(
+    "contain",
+    "Endereço de e-mail desconhecido"
+  ); // Verifica se o usuário está logado
+});
+
+it("deve exibir mensagem de erro ao inserir senha inválida", () => {
+  cy.visit("http://lojaebac.ebaconline.art.br/minha-conta");
+  cy.get("#username").type("alu@teste.com"); // Digita o usuário
+  cy.get("#password").type("teste@teste.com"); // Digita a senha
+  cy.get(".woocommerce-form > .button").click(); // Clica no botão de login
+  cy.get(".woocommerce-error").should(
+    "contain",
+    "Erro: a senha fornecida para o e-mail"
+  ); // Verifica se o usuário está logado
+});
